592cfaa80ea06456ec32fd3475ddf197bcb9dfa9
munishchoudhary204 <munishchoudhary204@gmail.com>
2022-12-20 15:32:43 +0530

    added rule edit: details,mapto

--- src/components/superadmin/Law/TabsSections/Rule/EditRuleDialog.js
+++ src/components/superadmin/Law/TabsSections/Rule/EditRuleDialog.js
@@ -27,21 +27,72 @@ import draftToHtml from "draftjs-to-html";
 import { EditorState, convertToRaw, ContentState } from "draft-js";
 
 import { useDispatch, useSelector } from "react-redux";
-import { editRule } from "../../../../../redux/superAdminReducer/superAdminAction";
+import {
+  editRule,
+  getDataTree,
+} from "../../../../../redux/superAdminReducer/superAdminAction";
 import { CKEditor } from "ckeditor4-react";
+import CheckboxTree from "react-checkbox-tree";
+import "react-checkbox-tree/lib/react-checkbox-tree.css";
 
 const EditRuleDialog = (props) => {
   const ruleId = props.rulesDetails._id;
-  const copyData = props?.ruleDetails;
+  const copyData = props?.rulesDetails;
+
+  const { dataTree } = useSelector((state) => state?.SuperAdmin);
 
   console.log(props);
 
-  console.log(props.rulesDetails.sub_sections);
+  console.log(props.rulesDetails);
+
+  console.log(copyData);
+  const checkedData = [
+    ...copyData?.law,
+    ...copyData?.act,
+    ...copyData?.chapter,
+    ...copyData?.section,
+    ...copyData?.subsection,
+  ];
+
+  function dfs(o, target) {
+    if (o.value === target) return [target];
+    if (!o.children) return false;
+    let path;
+    o.children.find((x) => (path = dfs(x, target)));
+    if (path) {
+      return [o.value].concat(path);
+    }
+  }
+  let path;
+  // dataTree.find((x) => (path = dfs(x, "63995983506bac3aee929016")));
+  // console.log(path);
+
+  const expandedDataArr = [];
+  checkedData.map((value) => {
+    dataTree.find((x) => (path = dfs(x, value)));
+    if (path?.length > 0) {
+      expandedDataArr.push(...path);
+    }
+  });
+  console.log(expandedDataArr);
 
   const dispatch = useDispatch();
 
   const [rule, setrule] = useState(props?.rulesDetails?.rule_name);
   const [value, setValue] = useState(props?.rulesDetails?.rule_details);
+
+  // tree data implemented
+  const [checked, setchecked] = useState(checkedData);
+  const [expanded, setexpanded] = useState(expandedDataArr);
+  const onCheck = (checked) => {
+    console.log(checked);
+
+    setchecked(checked);
+  };
+
+  const onExpand = (expanded) => {
+    setexpanded(expanded);
+  };
 
   const handleDialogClose = () => {
     props.setOpenDialog(false); // Use the prop.
@@ -52,6 +103,8 @@ const EditRuleDialog = (props) => {
     console.log("hey");
     const data = {
       rule_name: rule,
+      rule_details: value,
+      mapTo: checked,
     };
     console.log(data);
     await dispatch(editRule(data, ruleId));
@@ -68,6 +121,10 @@ const EditRuleDialog = (props) => {
     }
   }, [copyData]);
 
+  useEffect(() => {
+    dispatch(getDataTree());
+  }, []);
+
   return (
     <>
       {/* add admins dialog */}
@@ -80,7 +137,7 @@ const EditRuleDialog = (props) => {
           style: { borderRadius: 10 },
         }}
         fullWidth
-        maxWidth="sm"
+        maxWidth="md"
       >
         <DialogTitle fontWeight={600}>Edit Rule </DialogTitle>
         <Box position="absolute" top={5} right={10}>
@@ -122,7 +179,7 @@ const EditRuleDialog = (props) => {
 
                 <CKEditor
                   config={{
-                    readOnly: true,
+                    // readOnly: true,
                     allowedContent: true,
                     // forceEnterMode: true,
                     enterMode: "p",
@@ -200,6 +257,45 @@ const EditRuleDialog = (props) => {
                   }}
                 />
               </Box>
+
+              <FormControl
+                sx={{
+                  mt: 3,
+                  borderRadius: "6px",
+                  ".dropdown": {
+                    width: "100%",
+                    ".dropdown-trigger ": {
+                      width: "100%",
+                      borderRadius: "4px",
+                      ".tag-list .tag-item": {
+                        width: "93%",
+                      },
+                    },
+                  },
+
+                  ".dropdown-content": {
+                    maxHeight: "420px",
+                    overflowY: "auto",
+                    minWidth: "100%",
+                  },
+                }}
+              >
+                <Typography sx={{ mb: 1 }}>Map To</Typography>
+                {dataTree && (
+                  <CheckboxTree
+                    // disabled
+                    showExpandAll
+                    noCascade
+                    nodes={dataTree}
+                    checkModel="all"
+                    checked={checked}
+                    expanded={expanded}
+                    iconsClass="fa5"
+                    onCheck={onCheck}
+                    onExpand={onExpand}
+                  />
+                )}
+              </FormControl>
             </Box>
 
             <Box
